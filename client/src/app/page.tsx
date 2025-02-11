181fbd22481cc84bf547d7237bd71e49365121d0
hyonun321 <hyo_nun@kakao.com>
2025-02-11 16:56:52 +0900

    refactor: 프로젝트 pin 섹션 삭제
    - 모바일뷰에서 사용성이 너무 안좋음

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
-import ProjectSection2 from "@/components/ProjectSection2/ProjectSection2";
 import IntroductionSection from "@/components/IntroductionSection/IntroductionSection";
 import SpaceBackground from "@/components/common/SpaceBackground";
-import ProjectSection1 from "@/components/ProjectSection1/ProjectSection1";
 import ProjectGridSection from "@/components/ProjectGridSection/ProjectGridSection";
 import ProfessionalExperienceSection from "@/components/ProfessionalExperienceSection/ProfessionalExperienceSection";
 export default function HomePage() {
@@ -11,10 +9,10 @@ export default function HomePage() {
       <HeroSection />
       <IntroductionSection />
       <ProjectGridSection />
-      <div className="project-details">
+      {/* <div className="project-details">
         <ProjectSection1 />
         <ProjectSection2 />
-      </div>
+      </div> */}
       <ProfessionalExperienceSection />
       <SpaceBackground />
     </main>
