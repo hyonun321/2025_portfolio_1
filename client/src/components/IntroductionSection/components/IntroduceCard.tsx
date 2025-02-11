f1464227b09b725b86d6bf48bade05267e7bf6d0
hyonun321 <hyo_nun@kakao.com>
2025-02-11 14:04:37 +0900

    style: 글자색 변경

--- client/src/components/IntroductionSection/components/IntroduceCard.tsx
+++ client/src/components/IntroductionSection/components/IntroduceCard.tsx
@@ -4,7 +4,7 @@ export default function IntroduceCard() {
   return (
     <div className="m-auto px-4">
       <h3 className="flex flex-col items-center">
-        <span className="text-2xl md:text-5xl font-semibold text-gradient-purple text-center pt-5 pb-5">
+        <span className="text-2xl md:text-5xl font-semibold text-white text-center pt-5 pb-5">
           협업하고 싶은 개발자
         </span>
         <p className="text-white text-sm md:text-2xl text-left backdrop-blur-md bg-zinc-900/50 rounded-xl p-4 md:p-5">
