414379b321afa0deb22f57609d5bf75c62895e30
hyonun321 <hyo_nun@kakao.com>
2025-02-12 16:34:23 +0900

    style: 글자 크기 수정
    - 데스크탑 뷰에서 강조 글씨보다 일반글씨가 큰 문제

--- client/src/components/IntroductionSection/components/IntroduceCard.tsx
+++ client/src/components/IntroductionSection/components/IntroduceCard.tsx
@@ -7,7 +7,7 @@ export default function IntroduceCard() {
         <span className="text-2xl md:text-5xl font-semibold text-white text-center pt-5 pb-5">
           협업하고 싶은 개발자
         </span>
-        <p className="text-white text-sm md:text-2xl text-left backdrop-blur-md bg-zinc-900/50 rounded-xl p-4 md:p-5">
+        <p className="text-white text-sm md:text-xl text-left backdrop-blur-md bg-zinc-900/50 rounded-xl p-4 md:p-5">
           <Highlight1>사용자 경험을 최우선으로 생각</Highlight1>하며, <br />
           프로젝트에 <Highlight2>강한 Ownership</Highlight2> 을 가지고 있습니다.
           <br />
