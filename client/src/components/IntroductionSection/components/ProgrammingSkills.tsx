daddf664f0d32d5b270197435a988b55e761c3df
hyonun321 <hyo_nun@kakao.com>
2025-04-08 16:13:45 +0900

    refactor: skills data 코드 스플리팅

--- client/src/components/IntroductionSection/components/ProgrammingSkills.tsx
+++ client/src/components/IntroductionSection/components/ProgrammingSkills.tsx
@@ -2,14 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { FaReact } from "react-icons/fa";
-import {
-  SiJavascript,
-  SiTypescript,
-  SiSass,
-  SiTailwindcss,
-  SiStyledcomponents,
-} from "react-icons/si";
+import { skills } from "./data/programmingSkills";
 
 export default function ProgrammingSkills() {
   const containerRef = useRef(null);
@@ -28,45 +21,6 @@ export default function ProgrammingSkills() {
       fillPercent: "85%",
     },
   };
-
-  const skills = [
-    {
-      name: "React",
-      icon: <FaReact />,
-      level: "COMFORTABLE",
-      color: "#61DAFB",
-    },
-    {
-      name: "JavaScript",
-      icon: <SiJavascript />,
-      level: "COMFORTABLE",
-      color: "#F7DF1E",
-    },
-    {
-      name: "TypeScript",
-      icon: <SiTypescript />,
-      level: "COMFORTABLE",
-      color: "#3178C6",
-    },
-    {
-      name: "styled-components",
-      icon: <SiStyledcomponents />,
-      level: "COMFORTABLE",
-      color: "#DB7093",
-    },
-    {
-      name: "SCSS",
-      icon: <SiSass />,
-      level: "INTERMEDIATE",
-      color: "#CC6699",
-    },
-    {
-      name: "Tailwind",
-      icon: <SiTailwindcss />,
-      level: "INTERMEDIATE",
-      color: "#06B6D4",
-    },
-  ];
 
   useEffect(() => {
     gsap.fromTo(
---
+++ client/src/components/IntroductionSection/components/data/programmingSkills.tsx
@@ -0,0 +1,47 @@
+import { FaReact } from "react-icons/fa";
+import {
+  SiJavascript,
+  SiTypescript,
+  SiSass,
+  SiTailwindcss,
+  SiStyledcomponents,
+} from "react-icons/si";
+
+export const skills = [
+  {
+    name: "React",
+    icon: <FaReact />,
+    level: "COMFORTABLE",
+    color: "#61DAFB",
+  },
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />,
+    level: "COMFORTABLE",
+    color: "#F7DF1E",
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript />,
+    level: "COMFORTABLE",
+    color: "#3178C6",
+  },
+  {
+    name: "styled-components",
+    icon: <SiStyledcomponents />,
+    level: "COMFORTABLE",
+    color: "#DB7093",
+  },
+  {
+    name: "SCSS",
+    icon: <SiSass />,
+    level: "INTERMEDIATE",
+    color: "#CC6699",
+  },
+  {
+    name: "Tailwind",
+    icon: <SiTailwindcss />,
+    level: "INTERMEDIATE",
+    color: "#06B6D4",
+  },
+];
